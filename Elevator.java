ee05a6007c737c86b046bf2155350f4f045bc9db
sjoelee <sammy.joe.lee@gmail.com>
2014-04-06 08:30:30 -0700

    Removed setGoalFloorHelper because it only incremented the idx of the array in both up and down directions. Need to fix how downReqExists and upReqExists is being set though. Too messy.

--- Elevator.java
+++ Elevator.java
@@ -4,7 +4,7 @@ public class Elevator {
     int id;
     int dir; //-1 = down, +1 = up, 0 = stationary
     private int curr_floor;
-    private int goal_floor;
+    private int goal_floor; // -1 = no goal set
     private int max_floors;
     private boolean downReqExists;
     private boolean upReqExists;
@@ -16,11 +16,11 @@ public class Elevator {
     private boolean downRequests[];
 
     public Elevator (int init_id, int init_max_floors) { //constructor 
-        id = init_id;
-        curr_floor = 0;
-        goal_floor = -1; // no goal is set
-        dir = 0;
-        upReqExists = false;
+        id            = init_id;
+        curr_floor    = 0;
+        goal_floor    = -1;
+        dir           = 0;
+        upReqExists   = false;
         downReqExists = false;
 
         if (init_max_floors > 0) {
@@ -54,18 +54,21 @@ public class Elevator {
     }
 
     public void getState() {
-        System.out.println("Elevator " + id + " is on floor " + curr_floor + " has direction " + dir);
+        System.out.println("Elevator " + id + " is on floor " + 
+                           curr_floor + " has direction " + dir);
     }
 
-    private void setDirection (int set_dir) {
-        if (set_dir < -1 || set_dir > 1) {
-            System.out.println("Invalid direction!");
+    private void setDirection () {
+        System.out.println("Checking direction");
+        if (upReqExists) {
+            dir = 1;
+            System.out.println("Direction set to up");
             return;
-        }
-        if (dir == 0) {
-            dir = set_dir;
-        } else {
-            System.out.println("Elevator is already in motion");
+        } 
+        if (downReqExists) {
+            dir = -1;
+            System.out.println("Direction set to down");
+            return;
         }
 
         return;
@@ -73,66 +76,71 @@ public class Elevator {
 
     /* Modify boolean array */
     public void enqueueFloorRequest (int desired_floor) {
-        desired_floor--; //Offset by 1, because ground floor is at 0, not 1.
-        if (dir == 0) {
-            if (desired_floor - curr_floor > 0) {
-                dir = 1;
-            } else {
-                dir = -1;
-            }
-        }
-        switch (desired_floor) {
-        case 1: 
+        boolean isAboveCurrFloor = (desired_floor - curr_floor > 0) ? true : false;
+        if (isAboveCurrFloor) {
             upRequests[desired_floor] = true;
-            break;
-        case -1:
+            if (upReqExists == false) {
+                upReqExists = true;
+            }
+        } else {
             downRequests[desired_floor] = true;
-            break;
-        default:
-            break;
-        }
-    }
-
-    private int setGoalFloorHelper (boolean[] requests) {
-        int idx;
-        for (idx = curr_floor + 1; idx < max_floors; idx++) {
-            if (requests[idx] == true) {
-                return idx;
+            if (downReqExists == false) {
+                System.out.println("downreqExists set");
+                downReqExists = true;
             }
         }
-        return -1;
     }
 
-    private void setGoalFloor (int dir) {
+    private void setGoalFloor () {
+        int idx;
         switch (dir) {
         case 1:
-            goal_floor = setGoalFloorHelper(upRequests);
+            for (idx = curr_floor + 1; idx < max_floors; idx += 1) {
+                if (upRequests[idx] == true) {
+                    goal_floor = idx;
+                    return;
+                }
+            }
+            upReqExists = false;
+            goal_floor = -1;
+            dir = 0;
             break;
         case -1:
-            goal_floor = setGoalFloorHelper(downRequests);
+            for (idx = curr_floor - 1; idx >= 0; idx -= 1) {
+                if (downRequests[idx] == true) {
+                    goal_floor = idx;
+                    return;
+                }
+            }
+            downReqExists = false;
+            goal_floor = -1;
+            dir = 0;
             break;
         default:
             /* No floors to go to in direction it's currently in. Stay put. */
             goal_floor = -1;
             break;
         }
+        return;
     }
 
-    private void advanceToNextFloor (int dir) {
+    private void advanceToNextFloor () {
         /* What if the current floor is at the topmost or bottommost floor? */
         switch (dir) {
         case 1:
             if (curr_floor == max_floors - 1) { //Topmost floor
+                upReqExists = false;
                 dir = (downReqExists) ? -1 : 0;
-                setGoalFloor(dir);
+                setGoalFloor();
             } else {
                 curr_floor++;
             }
             break;
         case -1:
             if (curr_floor == 0) { //Ground floor
+                downReqExists = false;
                 dir = (upReqExists) ? 1 : 0;
-                setGoalFloor(dir);
+                setGoalFloor();
             } else {
                 curr_floor--;
             }
@@ -144,10 +152,10 @@ public class Elevator {
 
     /* Determines whether or not elevator is at desired floor. If so, mark
      * request as false (i.e. finished) and set next request. */
-    private void checkRequest (int dir, boolean[] requests) {
+    private void checkRequest (boolean[] requests) {
         if (curr_floor == goal_floor) {
             requests[curr_floor] = false;
-            setGoalFloor(dir);
+            setGoalFloor();
         } 
     }
 
@@ -168,19 +176,20 @@ public class Elevator {
      *  - if no, keep desired floor.
      */
     public void advance() {
-        advanceToNextFloor(dir);
+        advanceToNextFloor();
 
         if (getGoalFloor() == -1) {
-            setGoalFloor(dir);
+            setDirection();
+            setGoalFloor();
             return;
         }
 
         switch (dir) {
         case 1:
-            checkRequest(dir, upRequests);
+            checkRequest(upRequests);
             break;
         case -1:
-            checkRequest(dir, downRequests);
+            checkRequest(downRequests);
             break;
         default:
             break;
--- ElevatorTest.java
+++ ElevatorTest.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 public class ElevatorTest {
-    int max_floors = 5;
+    int max_floors = 6; // 0 = ground floor
     public static void main(String[] args) {
         int curr_floor;
         int dir;
 
-        Elevator e = new Elevator(0, 5);
-        e.getState();
+        Elevator e = new Elevator(0, 6);
+        /* Console console = System.console(); */
+        /* String input = console.readLine("Enter input:"); */
         e.enqueueFloorRequest(5);
-        e.advance();
-        e.getState();
-        e.advance();
-        e.getState();
+        e.enqueueFloorRequest(1);
+        int idx;
+        for (idx = 0; idx < 10; idx++) {
+            e.getState();
+            System.out.println("Current goal: " + e.getGoalFloor());
+            e.advance();
+            if (idx == 5) {
+                e.enqueueFloorRequest(3);
+            }
+        }
     }
 }
